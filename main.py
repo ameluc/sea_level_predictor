e780ea4d6fc46b6732bd2ec4c2ffa269bc8ac0f0
Améluc Ahognidjè <ameluc.ahognidje@protonmail.com>
2026-02-21 00:40:54 +0100

    feat(main): implemented app entrypoint for testing.

--- main.py
+++ main.py
@@ -8,5 +8,14 @@
 
  Author : Améluc Ahognidjè <ameluc.ahognidje@protonmail.com>
  Date : 2026-02-20
- Version : 0.0.0
+ Version : 1.0.0
 """
+
+from unittest import main
+import sea_level_predictor
+
+# Testing the function by calling it here
+sea_level_predictor.draw_plot()
+
+# Running unit tests automatically
+main(module="test_module", exit=False)
